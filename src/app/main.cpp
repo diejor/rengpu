92c672d6d733ddb16d8051106c36f5b276b8e1a2
diejor <diejorarr@gmail.com>
2025-02-10 18:28:39 -0600

    instancer

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,24 +1,28 @@
-#include <iostream>
 #include <webgpu/webgpu.h>
+
+#include <iostream>
 
 using namespace std;
 
 int main() {
-  // We create a descriptor
-  WGPUInstanceDescriptor desc = {};
-  desc.nextInChain = nullptr;
+    // We create a descriptor
+    WGPUInstanceDescriptor desc = {};
+    desc.nextInChain = nullptr;
 
-  // We create the instance using this descriptor
-  WGPUInstance instance = wgpuCreateInstance(&desc);
+    #ifdef WEBGPU_BACKEND_EMSCRIPTEN
+    WGPUInstance instance = wgpuCreateInstance(nullptr);
+    #else 
+    WGPUInstance instance = wgpuCreateInstance(&desc);
+    #endif
 
-  // We can check whether there is actually an instance created
-  if (!instance) {
-    std::cerr << "Could not initialize WebGPU!" << std::endl;
-    return 1;
-  }
+    // We can check whether there is actually an instance created
+    if (!instance) {
+        std::cerr << "Could not initialize WebGPU!" << std::endl;
+        return 1;
+    }
 
-  // Display the object (WGPUInstance is a simple pointer, it may be
-  // copied around without worrying about its size).
-  std::cout << "WGPU instance: " << instance << std::endl;
-  return 0;
+    // Display the object (WGPUInstance is a simple pointer, it may be
+    // copied around without worrying about its size).
+    std::cout << "WGPU instance: " << instance << std::endl;
+    return 0;
 }
