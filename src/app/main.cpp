94e86b914449234fb9867d46390f517ac3dffa9b
diejor <diejorarr@gmail.com>
2025-02-12 15:38:22 -0600

    mend

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -7,21 +7,65 @@ include<emscripten.h>
 #include <cassert>
 #include <iostream>
 
+    WGPUAdapter requestAdapterSync(WGPUInstance instance,
+                                   WGPURequestAdapterOptions const* options) {
+    struct UserData {
+        WGPUAdapter adapter = nullptr;
+        bool requestEnded = false;
+    };
+    UserData userData;
+
+    WGPURequestAdapterCallback ondapterRequestEnded =
+        [](WGPURequestAdapterStatus status, WGPUAdapter adapter,
+           WGPUStringView message, void* userdata1, [[maybe_unused]] void* userdata2) {
+            UserData& userData = *reinterpret_cast<UserData*>(userdata1);
+            if (status == WGPURequestAdapterStatus_Success) {
+                std::cout << "Adapter request succeeded!" << std::endl;
+                userData.adapter = adapter;
+            } else {
+                std::cout << "Could not get WebGPU adapter: "
+                          << (char*)message.data << std::endl;
+            }
+            userData.requestEnded = true;
+        };
+
+    WGPURequestAdapterCallbackInfo callbackInfo = {};
+    callbackInfo.nextInChain = nullptr;
+    callbackInfo.mode = WGPUCallbackMode_WaitAnyOnly;
+    callbackInfo.callback = ondapterRequestEnded;
+    callbackInfo.userdata1 = (void*)&userData;
+
+    std::cout << "Requesting adapter..." << std::endl;
+    // wgpuInstanceRequestAdapter(instance, options, callbackInfo);
+    WGPUFuture future = wgpuInstanceRequestAdapter(instance, options, callbackInfo);
+
+    WGPUFutureWaitInfo waitInfo = {};
+    waitInfo.future = future;
+    waitInfo.completed = false;
+    
+    wgpuInstanceWaitAny(instance, 1, &waitInfo, 0);
+
+
+    return userData.adapter;
+}
 
 int main() {
     // We create a descriptor
     WGPUInstanceDescriptor desc = {};
     desc.nextInChain = nullptr;
 
-#ifdef WEBGPU_BACKEND_EMSCRIPTEN
-    WGPUInstance instance = wgpuCreateInstance(nullptr);
-#else
     WGPUInstance instance = wgpuCreateInstance(&desc);
-#endif
 
     if (!instance) {
         std::cerr << "Could not initialize WebGPU!" << std::endl;
         return 1;
     }
+
+    WGPURequestAdapterOptions adapterOpts = {};
+    adapterOpts.nextInChain = nullptr;
+    WGPUAdapter adapter = requestAdapterSync(instance, &adapterOpts);
+
+    std::cout << "Got adapter: " << adapter << std::endl;
+
     return 0;
 }
